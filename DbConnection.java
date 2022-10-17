043019482926623daa49fdc0abc8e543567d1825
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-17 19:33:03 +0530

    Java

--- DbConnection.java
+++ DbConnection.java
@@ -7,13 +7,18 @@
 // 6. Process the result
 // 7. Close connection
 
-// Importing the java Sql package
+// Step 1 - Importing the java Sql package
 
 import java.sql.*;
 
 public class DbConnection {
     public static void main(String[] args) throws Exception {
         // we need to have the throws exception command in order to catch the sql errors
+
+        // Step 2 - Load and register the driver
+        // We could have used the TYPE 1 driver but the new version of JAVA, does not
+        // support the TYPE 1 for sql connections so we need to download the SQL Driver
+        // separately which is - "com.mysql.jdbc.Driver" which is a type 4
 
     }
 
