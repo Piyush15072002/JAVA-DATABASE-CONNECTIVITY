0b8d5dba65db73ef3f13162398c51d063d54848d
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-17 18:17:56 +0530

    Java

---
+++ DbConnection.java
@@ -0,0 +1,8 @@
+
+public class DbConnection {
+    public static void main(String[] args) throws Exception {
+        // we need to have the throws exception command in order to catch the sql errors
+
+    }
+
+}
