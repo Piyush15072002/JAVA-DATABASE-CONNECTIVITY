19f63a485ebc5474c329a7973452cfcdeafd94d6
PIYUSH PANT <piyushpant15@gmail.com>
2022-10-17 18:27:23 +0530

    JAVA

--- DbConnection.java
+++ DbConnection.java
@@ -1,3 +1,15 @@
+// * Step-Cycle for the connectivity
+// 1. Import
+// 2. Load and register the Driver
+// 3. Create connection
+// 4. Create a statement
+// 5. Execute the query
+// 6. Process the result
+// 7. Close connection
+
+// Importing the java Sql package
+
+import java.sql.*;
 
 public class DbConnection {
     public static void main(String[] args) throws Exception {
